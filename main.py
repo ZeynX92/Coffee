329eb5ae7f78771dd9a6ceed6ffd4e9066d41c69
ZeynX92 <sasha.sh.1980@mail.ru>
2022-12-29 20:45:37 +0300

    __init__

---
+++ main.py
@@ -0,0 +1,42 @@
+import csv
+import sys
+import sqlite3
+from PyQt5 import uic
+from random import randint
+from PyQt5.QtGui import QColor, QPixmap
+from PyQt5.QtWidgets import QApplication
+from PyQt5.QtWidgets import QMainWindow, QTableWidgetItem, QPushButton, QListView, QWidget
+
+
+class MyWidget(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('main.ui', self)
+
+        self.con = sqlite3.connect("coffee.sqlite")
+        self.titles = None
+
+        self.setup_table()
+
+    def setup_table(self):
+        cur = self.con.cursor()
+        result = None
+
+        result = cur.execute(
+            f"""SELECT coffee.name, coffee.roasting_lvl, coffee.beans, coffee.taste, coffee.price, coffee.volume 
+                        FROM coffee""").fetchall()
+
+        self.tableWidget.setRowCount(len(result))
+        self.tableWidget.setColumnCount(len(result[0]))
+        self.titles = ["Название", "Степень обжарки", "В зернах?", "Описание вкуса", "Стоимость пачки", "Объем пачки"]
+        self.tableWidget.setHorizontalHeaderLabels(self.titles)
+        for i, elem in enumerate(result):
+            for j, val in enumerate(elem):
+                self.tableWidget.setItem(i, j, QTableWidgetItem(str(val)))
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = MyWidget()
+    ex.show()
+    sys.exit(app.exec())
